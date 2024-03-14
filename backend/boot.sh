249b23c4c412cf94dbb11e05599a66d73b9a1725
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2024-03-14 13:35:51 -0300

    first commit

---
+++ backend/boot.sh
@@ -0,0 +1,2 @@
+source $(find . -type f -name activate | grep .)
+python3 app.py
---
+++ backend/install.sh
@@ -0,0 +1,14 @@
+activate=$(find . -type f -name activate | grep .)
+
+if [ $? -eq "0" ] ; then
+    echo '[+] Entorno existente encontrado...'
+else
+    echo '[+] Creando entorno virtual de nombre env'
+    python3 -m venv env
+    activate="env/bin/activate"
+fi
+
+echo '[+] Activando entorno virtual'
+source $activate
+echo '[+] Instalando dependencias'
+pip3 install -r requirements.txt  &>/dev/null 
