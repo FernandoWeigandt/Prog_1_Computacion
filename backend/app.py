225baef69fccd845aae3367afe456a89005e81bc
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2024-03-21 10:28:49 -0300

    Add a global configuration to start flask and get app variable in all the modules.

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-from dotenv import load_dotenv
+from main import create_app
 import os
 
-load_dotenv()
-app = Flask(__name__)
+# retorno de funcion __init__.py
+app = create_app()
+
+app.app_context().push()
 
 if __name__ == '__main__':
     app.run(debug=True,port=os.getenv('PORT'))
