0d54d51b58c68d9a4246ced9db144a2bb137fc4b
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2024-04-01 04:30:32 -0300

    Add Book and Books resources. Modify the postman collection to get all the resources in one collection. Add empty files for the missing resources.

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -9,7 +9,11 @@ api = Api()
 def create_app():
     app=Flask(__name__)
     load_dotenv()
+    #User
     api.add_resource(resources.UsersResources, '/users')
     api.add_resource(resources.UserResources, '/user/<id>')
+    #Book
+    api.add_resource(resources.BooksResources, '/books')
+    api.add_resource(resources.BookResources, '/book/<id>')
     api.init_app(app)
     return app
